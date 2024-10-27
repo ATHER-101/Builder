0b50c00eeeb352f97d768edf94ac5f0ae0c6b17a
ATHER-101 <atharvatijare04@gmail.com>
2024-10-27 12:04:54 +0530

    Added new project

--- index.js
+++ index.js
@@ -1,37 +1,23 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // Middleware for parsing JSON
 app.use(express.json());
-
-// // Basic route to handle GET requests
-// app.get('/', (req, res) => {
-//   res.send({name:'Hello World from Express!'});
-// });
-
-// // Route with a dynamic parameter
-// app.get('/hello/:name', (req, res) => {
-//   const name = req.params.name;
-//   res.send(`Hello, ${name}!`);
-// });
-
-// // Route to handle POST requests
-// app.post('/data', (req, res) => {
-//   const receivedData = req.body;
-//   res.json({
-//     message: 'Data received successfully!',
-//     data: receivedData,
-//   });
-// });
-
-// Import the routes from the separate file
-const routes_1234_01 = require('./routes/1234/01');
-
-// Use the imported routes
-app.use('/1234/01', routes_1234_01);
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+
+const routes_1234_01 = require('./routes/1234/01');
+app.use('/1234/01', routes_1234_01);
+
+const routes_1234_02 = require('./routes/1234/02');
+app.use('/1234/02', routes_1234_02);
+
+
+const routes_1234_03 = require('./routes/1234/03');
+app.use('/1234/03', routes_1234_03);
+
