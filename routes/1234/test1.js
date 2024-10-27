5f8d8db380c3c50a5a71f1247fa01ddc761e3b1c
ATHER-101 <atharvatijare04@gmail.com>
2024-10-27 14:52:56 +0530

    Update 01.js with new route handler

--- routes/1234/test1.js
+++ routes/1234/test1.js
@@ -13,3 +13,18 @@ router.get('/user5', (req, res) => {
     { "user_id": "user5", "name": "Bob Builder", "age": 35 }
   ]);
 });
+
+router.get('/user2', (req, res) => {
+  res.json([
+    {
+      "user_id": "user123",
+      "name": "John Doe",
+      "age": 30
+    },
+    {
+      "user_id": "user456",
+      "name": "Jane Doe",
+      "age": 25
+    }
+  ]);
+});
