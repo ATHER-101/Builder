56a1dd0ee1c7ca9229620f0528872fa86364f9d0
ATHER-101 <atharvatijare04@gmail.com>
2024-10-27 13:03:36 +0530

    Added new project

--- index.js
+++ index.js
@@ -11,21 +11,10 @@ app.listen(port, () => {
 });
 
 
-const routes_1234_01 = require('./routes/1234/01');
-app.use('/1234/01', routes_1234_01);
-
-const routes_1234_02 = require('./routes/1234/02');
-app.use('/1234/02', routes_1234_02);
-
-
-const routes_1234_03 = require('./routes/1234/03');
-app.use('/1234/03', routes_1234_03);
-
-
 const routes_1234_test1 = require('./routes/1234/test1');
 app.use('/1234/test1', routes_1234_test1);
 
 
-const routes_1234_test1 = require('./routes/1234/test1');
-app.use('/1234/test1', routes_1234_test1);
+const routes_atharva.tijare_test2 = require('./routes/atharva.tijare/test2');
+app.use('/atharva.tijare/test2', routes_atharva.tijare_test2);
 
