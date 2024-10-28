66ac66302722731e8edd747aed06c85070a6a11e
ATHER-101 <atharvatijare04@gmail.com>
2024-10-29 03:16:01 +0530

    Update 01.js with new route handler

--- routes/2345/test6.js
+++ routes/2345/test6.js
@@ -3,3 +3,13 @@ const router = express.Router();
 
 module.exports = router;
 
+
+router.get('/user1', (req, res) => {
+  res.json([
+    {
+      "user_id": "user123",
+      "name": "John Doe",
+      "age": 30
+    }
+  ]);
+});
