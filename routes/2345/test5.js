18d80409a8457bbf40487971f2363612f8d1bb9b
ATHER-101 <atharvatijare04@gmail.com>
2024-10-29 00:14:42 +0530

    Update 01.js with new route handler

--- routes/2345/test5.js
+++ routes/2345/test5.js
@@ -3,3 +3,18 @@ const router = express.Router();
 
 module.exports = router;
 
+
+router.get('/random', (req, res) => {
+  res.json([
+    {
+      "book": "The Hitchhiker's Guide to the Galaxy",
+      "animal": "Dog",
+      "drink": 3
+    },
+    {
+      "book": "1984",
+      "animal": "Cat",
+      "drink": 2
+    }
+  ]);
+});
