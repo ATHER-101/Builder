8c0edb05a8e852cd6634d6fae8c04182fb8d1366
ATHER-101 <atharvatijare04@gmail.com>
2024-10-29 19:14:24 +0530

    added cors

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-// Middleware for parsing JSON
+app.use(cors());
+
 app.use(express.json());
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
