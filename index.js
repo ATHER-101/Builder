a071e0db27eb4f1a2ab82d20476be2dc077f5923
ATHER-101 <atharvatijare04@gmail.com>
2024-10-29 19:38:33 +0530

    Added new project

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ app.listen(port, () => {
 
 const routes_2345_test1 = require('./routes/2345/test1');
 app.use('/2345/test1', routes_2345_test1);
+
+const routes_2345_amit-singh = require('./routes/2345/amit-singh');
+app.use('/2345/amit-singh', routes_2345_amit-singh);
+
